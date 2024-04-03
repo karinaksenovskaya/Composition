863fe8647b98a0e0d28c823cc9468bd7da6c2f87
Karina <krin_ka@mail.ru>
2024-04-03 16:02:26 +0300

    Getting started with coroutines

---
+++ app/src/main/java/com/example/composition/domain/entity/GameResult.kt
@@ -0,0 +1,12 @@
+package com.example.composition.domain.entity
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class GameResult(
+    val winner: Boolean,
+    val countOfRightAnswer: Int,
+    val countOfQuestions: Int,
+    val gameSettings: GameSettings
+) : Parcelable
---
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -0,0 +1,67 @@
+package com.example.composition.presentation
+
+import android.R
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.composition.databinding.FragmentGameBinding
+import com.example.composition.domain.entity.GameResult
+
+class GameFragment : Fragment() {
+
+    private val args by navArgs<GameFragmentArgs>()
+
+    private val viewModelFactory by lazy {
+        GameViewModelFactory(args.level, requireActivity().application)
+    }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[GameViewModel::class.java]
+    }
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        observeViewModel()
+    }
+
+
+    private fun observeViewModel() {
+        viewModel.gameResult.observe(viewLifecycleOwner) {
+            launchGameFinishedFragment(it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        findNavController().navigate(
+            GameFragmentDirections.actionGameFragmentToGameFinishedFragment(gameResult)
+        )
+    }
+}
